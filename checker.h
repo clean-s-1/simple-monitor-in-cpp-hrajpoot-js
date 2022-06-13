900fbd81e803f46c3a75f02613edfe7d04ff1832
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-06-13 17:37:14 +0530

    Create checker.h

---
+++ checker.h
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+#define LOW_TEMPERATURE 0
+#define HIGH_TEMPERATURE 45
+#define LOW_SOC 20
+#define HIGH_SOC 80
+#define CHARGE_RATE 0.8
+
+
+class BatteryManagementSystem
+{
+    public:
+    float _temperature;
+    float _stateOfCharge;
+    float _chargeRate;
+
+    BatteryManagementSystem(float temperature, float stateOfCharge, float chargeRate) : 
+        _temperature(temperature), _stateOfCharge(stateOfCharge), _chargeRate(chargeRate) {}
+   
+    //BMS Member functions
+    bool batteryIsOk();
+    bool checkTemperature();
+    bool checkStateOfCharge();
+    bool checkChargeRate();
+};

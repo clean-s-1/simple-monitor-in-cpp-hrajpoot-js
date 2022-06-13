77819bd158f7f133bed8ad80c2bcf57047f54bb9
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-06-13 17:37:59 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,22 +1,52 @@
-#include <assert.h>
-#include <iostream>
+#include "checker.h"
+
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+bool BatteryManagementSystem::checkTemperature(){
+    bool returnValue = true;
+
+    if(_temperature < LOW_TEMPERATURE || _temperature > HIGH_TEMPERATURE) {
+        cout << "Temperature out of range!\n";
+        returnValue = false;
+    }
+
+    return returnValue;
+}
+
+bool BatteryManagementSystem::checkStateOfCharge(){
+    bool returnValue = true;
+
+    if(_stateOfCharge < LOW_SOC || _stateOfCharge > HIGH_SOC) {
+        cout << "State of Charge out of range!\n";
+        returnValue = false;
+    }
+
+    return returnValue;
+}
+
+bool BatteryManagementSystem::checkChargeRate(){
+    bool returnValue = true;
+    if(_chargeRate > CHARGE_RATE) {
+        cout << "Charge Rate out of range!\n";
+        returnValue = false;
+    }
+
+    return returnValue;
+}
+
+bool BatteryManagementSystem::batteryIsOk() {
+    return (checkTemperature() && checkStateOfCharge() && checkChargeRate());
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+
+  BatteryManagementSystem *bmsObj1 = new BatteryManagementSystem(25, 70, 0.7);
+  BatteryManagementSystem *bmsObj2 = new BatteryManagementSystem(50, 85, 0);
+  BatteryManagementSystem *bmsObj3 = new BatteryManagementSystem(1, 15, 0.1);
+  BatteryManagementSystem *bmsObj4 = new BatteryManagementSystem(44, 79, 0.9);
+
+  assert(bmsObj1->batteryIsOk() == true);
+  assert(bmsObj2->batteryIsOk() == false);
+  assert(bmsObj3->batteryIsOk() == false);
+  assert(bmsObj4->batteryIsOk() == false);
 }

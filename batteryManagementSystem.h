0f60d9e9224e7d22506fe6f25a2874f8202a488a
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-06-23 16:57:09 +0530

    Update batteryManagementSystem.h

--- batteryManagementSystem.h
+++ batteryManagementSystem.h
@@ -13,5 +13,22 @@ class BatteryManagementSystem {
     bool check_higher_limit(const float allowed_higher_limit, const float input_limit);
     void lower_abnormal_msg(const string message);
     void higher_abnormal_msg(const string message);
+    
+    void validate_boundry_conditions(const float number);
+
+    //data-structure with a series of numbers representing the boundary of each condition
+    map<pair<float, float>, string> boundry_conditions = {
+        { { 0, 20 }, "LOW_SOC_BREACH"},
+        { { 21, 24 }, "LOW_SOC_WARNING"},
+        { { 25, 75 }, "NORMAL" },
+        { { 76, 80 }, "HIGH_SOC_WARNING" },
+        { { 81, 100 }, "HIGH_SOC_WARNING" }
+    };
+
+   map<string, string> warning_message = {
+       {"LOW_SOC_WARNING", "Warning: Approaching discharge"},
+       {"HIGH_SOC_WARNING", "Warning: Approaching charge-peak"}
+   };
+
 };
 

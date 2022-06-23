1472ac98a727ecdd671343978b0b70b61b76c85c
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-06-23 16:58:51 +0530

    Update batteryManagementSystem.h

--- batteryManagementSystem.h
+++ batteryManagementSystem.h
@@ -7,7 +7,6 @@ class BatteryManagementSystem {
     public:
     virtual bool checkBattery() = 0;
     void raise_alert(const string message);
-    float fetch_warning_tolerance(const float upper_limit);
    
     bool check_lower_limit(const float allowed_lower_limit, const float input_limit);
     bool check_higher_limit(const float allowed_higher_limit, const float input_limit);
